44cd5b3777d75bac2a2e536243995fe0c3a52919
Sean Chapel <seoushi@gmail.com>
2010-09-24 21:05:02 +0000

    added linux sample projects 1-3 and fixed naming on project 4

--- samples/linux/01-Window-Creation/run-sample-01.sh
+++ samples/linux/01-Window-Creation/run-sample-01.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 # this is used to run the tutorial without having to install the library
-LD_LIBRARY_PATH=../../../projects/linux/ ./tut4
+LD_LIBRARY_PATH=../../../projects/linux/ ./sample-01
---
+++ samples/linux/02-Displaying-A-Triangle/run-sample-02.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+# this is used to run the tutorial without having to install the library
+LD_LIBRARY_PATH=../../../projects/linux/ ./sample-02
